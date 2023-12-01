26545a8d57e99ba921aee8fa774c3a269e279e44
LankShot <ajman5558@gmail.com>
2023-12-01 17:34:03 -0600

    Added, main scene in which player can move in. Added pits which player can collide with and added collissions to player script. Fall not yet implemented.

--- player.gd
+++ player.gd
@@ -6,6 +6,7 @@ var BASE_SPEED = 400 ##Global for base speed of the player. Global needed for ca
 @export var speed = BASE_SPEED
 var screen_size
 var sprint_multi = 1.5 ##calculates how much sprint will affect your speed.
+var health = 100
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -17,26 +18,46 @@ func start(pos):
 	$CollisionShape2D.disabled = false
 	
 func _process(delta):
+	check_collisions()
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("move_down"):
-		velocity.y -= 1
-	if Input.is_action_pressed("move_up"):
 		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
 		
 	if Input.is_action_pressed("sprint"):
-		position += velocity * delta
-		position = position.clamp(Vector2.ZERO, screen_size)
-		$AnimatedSprite2D.animation.set_speed_scale( float(speed*sprint_multi)/BASE_SPEED)
+		sprint_multi = 1.5
 	else:
-		position += velocity * delta
-		position = position.clamp(Vector2.ZERO, screen_size)
-		$AnimatedSprite2D.set_speed_scale( float(speed)/BASE_SPEED)
+		sprint_multi = 1
+	
+	if velocity.length() > 0:
+		
+		velocity = velocity.normalized() * speed * sprint_multi
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
 		
 	if velocity.x < 0:
 		$AnimatedSprite2D.flip_h = true
 	else:
 		$AnimatedSprite2D.flip_h = false
+	
+	##$AnimatedSprite2D.animation.set_speed_scale( float(speed*sprint_multi)/BASE_SPEED)
+	position += velocity * delta * sprint_multi
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+func check_collisions():
+	if(has_overlapping_areas()):
+		var objects = get_overlapping_areas()
+		for ob in objects: ##ob is a reference to an object in obects
+			match ob.is_in_group(x):
+				'Pit':
+					fall()
+		print_debug(objects)
+	
+func fall():
+	
